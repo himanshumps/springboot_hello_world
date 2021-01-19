1137e39856528762527523e232fc1036a9f21547
Himanshu Gupta <himanshu.mps@gmail.com>
2021-01-19 21:20:33 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -10,7 +10,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World from spring boot application !";
     }
 
     public static void main(String[] args) throws Exception {
